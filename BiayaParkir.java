91bd14d398601f0db31ea4e0c026f6b184c6aee8
unknown <rizky.maulana2351@gmail.com>
2023-10-04 20:01:03 +0700

    Pergantian Nama File

--- BiayaParkir.java
+++ BiayaParkir.java
@@ -1,5 +1,5 @@
 import  java.util.Scanner;
-public class Variasi_input {
+public class BiayaParkir {
     public static void main(String[]args){
       Scanner data = new Scanner(System.in);
       int tarif = 2000;
