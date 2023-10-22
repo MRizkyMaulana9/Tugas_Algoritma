9900307d03857d3814fe49b4f4b9ead17daca105
unknown <rizky.maulana2351@gmail.com>
2023-10-22 14:12:55 +0700

    Palindrome

--- Algoritma_Sesi_4/src/Palindrome.java
+++ Algoritma_Sesi_4/src/Palindrome.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Palindrom {
+public class Palindrome {
 	public static void main(String[] args) throws Exception {
 		Scanner input = new Scanner(System.in);
 
